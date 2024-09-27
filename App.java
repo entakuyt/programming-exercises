13eaa3b89756ed8960506116082fa8497661280a
YAN <your.email@example.com>
2024-09-27 19:30:40 +0200

    creat

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        String creator = "Yan";
+        System.out.println("printer App");
+        System.out.println(creator);
+
+
+    }
+}
